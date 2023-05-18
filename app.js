c6cc728afb01aa6c5a55124247748bceb0375d08
filipj00 <filip.janik00@gmail.com>
2023-05-18 17:04:21 +0200

    jobs done

--- app.js
+++ app.js
@@ -1,53 +1,89 @@
-function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors']
-    const randomIndex = Math.floor(Math.random() * choices.length)
-    return choices[randomIndex]
-}
 
+// Variables
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const info = document.querySelector('.info');
+const player = document.querySelector('#player');
+const computer = document.querySelector('#computer');
+const winBox = document.querySelector('.win-box');
+const playAgain = document.querySelector('.play-again');
+
+let roundCount = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+// Buttons with EventListeners
+rock.addEventListener('click', function() {
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
+paper.addEventListener('click', function() {
+    const playerSelection = "paper";
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
+scissors.addEventListener('click', function() {
+    const playerSelection = "scissors";
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
+// Function that allows to play single round.
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
-    computerSelection = computerSelection.toLowerCase()
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection === computerSelection) {
-        return "It's a tie!"
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
-               playerSelection === 'paper' && computerSelection === 'rock' ||
-               playerSelection === 'scissors' && computerSelection === 'paper') {
-      return `You win! ${playerSelection} beats ${computerSelection}.`
+        info.innerText = "It's a tie!";
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        playerScore++;
+        info.innerText = `You win! ${playerSelection} beats ${computerSelection}.`;
     } else {
-      return `You lose! ${computerSelection} beats ${playerSelection}.`
-    }       
-}
-
-
-function game() {
-    let playerScore = 0
-    let computerScore = 0
-
-    for (let i = 1; i <= 5; i++) {
-        const playerSelection = prompt("Choose Rock, Paper or Scissors: ").toLowerCase()
-        const computerSelection = getComputerChoice()
-        const result = playRound(playerSelection, computerSelection)
-        console.log(result)
-
-        if (result.startsWith("You win!")) {
-            playerScore++
-        } else if (result.startsWith("You lose!")) {
-            computerScore++
-        }
+        computerScore++;
+        info.innerText = `You lose! ${computerSelection} beats ${playerSelection}.`;
     }
 
-    console.log("--Final score--")
-    console.log(`Player score: ${playerScore}`)
-    console.log(`Computer score: ${computerScore}`)
+    roundCount++;
+    updateScores();
 
+    if (roundCount === 5) {
+        endGame();
+        playAgain.style.display = 'block';
+    }
+}
+
+// Function to update player and computer scores
+function updateScores() {
+    player.innerText = playerScore;
+    computer.innerText = computerScore;
+}
+
+// Function to end the game and display the result
+function endGame() {
     if (playerScore > computerScore) {
-        console.log("Congratulations! You win! :)")
+        winBox.innerText = "Congratulations! You win! :)";  
     } else if (playerScore < computerScore) {
-        console.log("Unfortunatelly you lose :(")
+        winBox.innerText = "Unfortunately you lose :(";
     } else {
-        console.log("It's a tie! Play again!")
+        winBox.innerText = "It's a tie!";
     }
+
+    // Disable buttons after the game ends
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
 }
 
-game()
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
